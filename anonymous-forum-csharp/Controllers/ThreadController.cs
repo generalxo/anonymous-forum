5818ed1902c740789c83088862084bb0f6ed7d86
rieerep <114030611+rieerep@users.noreply.github.com>
2023-08-31 18:09:41 +0200

    added controller for threads

---
+++ anonymous-forum-csharp/Controllers/ThreadController.cs
@@ -0,0 +1,16 @@
+﻿using anonymous_forum_csharp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace anonymous_forum_csharp.Controllers
+{
+    public class ThreadController : Controller
+    {
+        public IActionResult Index(string topicName)
+        {
+            var context = new ApplicationDbContext();
+            var post = context.Posts.ToList();
+            var viewModel = new List<>
+            return View();
+        }
+    }
+}

13e45c8ad981c2474ec00d4b792a7f195ad8db60
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-09-01 14:06:44 +0200

    Added Thread page

---
+++ anonymous-forum-csharp/Models/ViewModels/ThreadIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace anonymous_forum_csharp.Models.ViewModels
+{
+    public class ThreadIndexViewModel
+    {
+        public int Id { get; set;}
+        public string Title { get; set;}
+        public string Text { get; set;}
+
+    }
+}

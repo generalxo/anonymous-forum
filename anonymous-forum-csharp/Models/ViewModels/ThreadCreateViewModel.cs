962de614e80ac5a711d61ddd68830bc9aef679c1
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-09-01 16:02:52 +0200

    Added Post form

---
+++ anonymous-forum-csharp/Models/ViewModels/ThreadCreateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace anonymous_forum_csharp.Models.ViewModels
+{
+    public class ThreadCreateViewModel
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}

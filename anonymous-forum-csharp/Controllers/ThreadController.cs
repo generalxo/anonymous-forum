ab54d626c393b939e18c9860012946767b773b2b
Oliver Wasner <oli.wasner@hotmail.com>
2023-09-12 11:18:51 +0200

    save to db

--- anonymous-forum-csharp/Controllers/ThreadController.cs
+++ anonymous-forum-csharp/Controllers/ThreadController.cs
@@ -6,64 +6,65 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace anonymous_forum_csharp.Controllers
 {
-    public class ThreadController : Controller
-    {
-        private readonly IPostRepository _postRepository;
+	public class ThreadController : Controller
+	{
+		private readonly IPostRepository _postRepository;
 
-        public ThreadController(IPostRepository postRepository)
-        {
-            _postRepository = postRepository;
-        }
+		public ThreadController(IPostRepository postRepository)
+		{
+			_postRepository = postRepository;
+		}
 
-        [HttpGet]
-        public IActionResult Index(int id)
-        {
-            var posts = _postRepository.GetByCondition(x => x.TopicId == id);
-            if (!posts.Any())
-            {
-                // Handle error
-            }
+		[HttpGet]
+		public IActionResult Index(int id)
+		{
+			var posts = _postRepository.GetByCondition(x => x.TopicId == id);
+			if (!posts.Any())
+			{
+				// Handle error
+			}
 
-            var viewModel = CreateViewModel(posts);
-            
-            
-            string? ip = Helper.GetIp(HttpContext);
-            Console.WriteLine(ip);
-            return View(viewModel);
-        }
+			var viewModel = CreateViewModel(posts);
 
-        public IActionResult ThreadPostBox()
-        {
-            ThreadCreateViewModel viewModel = new();
-            return View(viewModel);
-        }
 
-        [HttpPost]
-        public IActionResult ThreadPostBox(ThreadCreateViewModel viewModel, int id)
-        {
-            Console.WriteLine($"id: {id}, text: {viewModel.Text}, title: {viewModel.Title}");
-            string? ip = Helper.GetIp(HttpContext);
-            var post = new PostModel
-            {
-                Title = viewModel.Title,
-                Text = viewModel.Text,
-                IpAdress = ip,
-                TopicId = id
-            };
-            _postRepository.Create(post);
+			string? ip = Helper.GetIp(HttpContext);
+			Console.WriteLine(ip);
+			return View(viewModel);
+		}
 
-            
+		public IActionResult ThreadPostBox()
+		{
+			ThreadCreateViewModel viewModel = new();
+			return View(viewModel);
+		}
 
-            return RedirectToAction("Index", new { id = id });
-        }
+		[HttpPost]
+		public IActionResult ThreadPostBox(ThreadCreateViewModel viewModel, int id)
+		{
+			Console.WriteLine($"id: {id}, text: {viewModel.Text}, title: {viewModel.Title}");
+			string? ip = Helper.GetIp(HttpContext);
+			var post = new PostModel
+			{
+				Title = viewModel.Title,
+				Text = viewModel.Text,
+				IpAdress = ip,
+				TopicId = id
+			};
+			_postRepository.Create(post);
+			_postRepository.Save();
 
-        private static ThreadIndexViewModel CreateViewModel(IQueryable<PostModel> posts)
-        {
-            var viewModel = new ThreadIndexViewModel
-            {
-                PostList = posts.Select(u => new PostModel { Id = u.Id, Title = u.Title, Text = u.Text, TopicId = u.TopicId }).ToList(),
-            };
-            return viewModel;
-        }
-    }
+
+
+			return RedirectToAction("Index", new { id = id });
+		}
+
+		private static ThreadIndexViewModel CreateViewModel(IQueryable<PostModel> posts)
+		{
+			var viewModel = new ThreadIndexViewModel
+			{
+				PostList = posts.Select(u => new PostModel { Id = u.Id, Title = u.Title, Text = u.Text, TopicId = u.TopicId }).ToList(),
+			};
+			return viewModel;
+		}
+	}
 }
